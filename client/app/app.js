05f0a7154052d2efac7face33c8286e0906cb0cb
Ozan Saral <Ozan Saral>
2017-08-08 15:27:13 +0300

    restangular added

--- client/app/app.js
+++ client/app/app.js
@@ -3,18 +3,33 @@ import uiRouter from 'angular-ui-router';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
+import restangular from 'restangular';
 import 'normalize.css';
 
 angular.module('app', [
     uiRouter,
     Common,
-    Components
+    Components,
+    restangular
   ])
   .config(($locationProvider) => {
     "ngInject";
     // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
     // #how-to-configure-your-server-to-work-with-html5mode
     $locationProvider.html5Mode(true).hashPrefix('!');
-  })
+  }).config((RestangularProvider) => {
+    "ngInject";
+    RestangularProvider.setBaseUrl(API_URL);
+    RestangularProvider.setDefaultHeaders({
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Allow-Control-Allow-Origin': '*'
+    });
+
+    RestangularProvider.setResponseExtractor(function (response, operation) {
+        return response.data;
+    });
+
+})
 
   .component('app', AppComponent);

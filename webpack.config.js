6bc4b8d68828743f5901315bc0f3486ee7c3da1e
Ozan Saral <Ozan Saral>
2017-08-08 16:21:11 +0300

    bootstrap added

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,33 @@ module.exports = {
   devtool: 'source-map',
   entry: {},
   module: {
-    loaders: [
-       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
-       { test: /\.html$/, loader: 'raw' },
-       { test: /\.(scss|sass)$/, loader: 'style!css!sass' },
-       { test: /\.css$/, loader: 'style!css' }
-    ]
+      loaders: [
+          {test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel'},
+          {test: /\.(png)$/, loader: 'url-loader?limit=100000'},
+          {
+              test: /\.svg(\?v=[0-9]\.[0-9]\.[0-9].[0-9].[0-9].[0-9].[0-9])?$/,
+              loader: 'url?limit=65000&mimetype=image/svg+xml&name=/[name].[ext]'
+          },
+          {
+              test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9].[0-9].[0-9].[0-9].[0-9])?$/,
+              loader: 'url?limit=65000&mimetype=application/font-woff&name=/[name].[ext]'
+          },
+          {
+              test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9].[0-9].[0-9].[0-9].[0-9])?$/,
+              loader: 'url?limit=65000&mimetype=application/font-woff2&name=/[name].[ext]'
+          },
+          {
+              test: /\.[ot]tf(\?v=[0-9]\.[0-9]\.[0-9].[0-9].[0-9].[0-9].[0-9])?$/,
+              loader: 'url?limit=65000&mimetype=application/octet-stream&name=/[name].[ext]'
+          },
+          {
+              test: /\.eot(\?v=[0-9]\.[0-9]\.[0-9].[0-9].[0-9].[0-9].[0-9])?$/,
+              loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=/[name].[ext]'
+          },
+          {test: /\.html$/, loader: 'raw'},
+          {test: /\.(scss|sass)$/, loader: 'style!css!sass'},
+          {test: /\.css$/, loader: 'style!css'}
+      ]
   },
   plugins: [
     // Injects bundles in your index.html instead of wiring all manually.
